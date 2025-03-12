e19afd7355254ddf38adf4775d2241125fd856ce
and-rs <deserd@protonmail.com>
2025-03-11 22:55:00 -0500

    fixed div height

--- frontend/src/app/(auth)/dashboard/layout.tsx
+++ frontend/src/app/(auth)/dashboard/layout.tsx
@@ -9,8 +9,9 @@ export default function DashboardLayout({
   children: React.ReactNode;
 }) {
   return (
-    <div className="flex flex-col w-full bg-background">
+    <div className="flex flex-col w-full min-h-screen bg-background">
       <Nav />
+
       <div className="flex overflow-hidden flex-col flex-1 py-32 md:py-24">
         <main className="overflow-y-auto flex-1 p-4 sm:p-6">{children}</main>
       </div>
